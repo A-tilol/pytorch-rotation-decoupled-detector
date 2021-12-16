ca51ca2f3a6bc3611a0c102c3ae1ce6eccf159fc
igarashi <igarashi.shunta@canon-elec.co.jp>
2021-12-16 10:19:47 +0900

    update

--- utils/box/ext/rbbox_overlap_cpu/__init__.py
+++ utils/box/ext/rbbox_overlap_cpu/__init__.py
@@ -1,10 +1,10 @@
-# import sys, os
-# print(os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_cpu"))
-# sys.path.append(
-#     os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_cpu"))
+import sys, os
+print(os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_cpu"))
+sys.path.append(
+    os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_cpu"))
 
-# from pprint import pprint
-# pprint(sys.path)
+from pprint import pprint
+pprint(sys.path)
 
 from rbbox_overlap import rbbox_iou
 from rbbox_overlap import rbbox_iou_1x1

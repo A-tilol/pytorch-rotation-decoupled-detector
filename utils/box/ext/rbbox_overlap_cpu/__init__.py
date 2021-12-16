780d712182ba9acbdd6df6356c59096484f5832c
igarashi <igarashi.shunta@canon-elec.co.jp>
2021-12-16 09:53:49 +0900

    update

--- utils/box/ext/rbbox_overlap_cpu/__init__.py
+++ utils/box/ext/rbbox_overlap_cpu/__init__.py
@@ -1,6 +1,4 @@
-
-
-from .rbbox_overlap import rbbox_iou
-from .rbbox_overlap import rbbox_iou_1x1
-from .rbbox_overlap import rbbox_iou_nxn
-from .rbbox_overlap import rbbox_nms
+from rbbox_overlap import rbbox_iou
+from rbbox_overlap import rbbox_iou_1x1
+from rbbox_overlap import rbbox_iou_nxn
+from rbbox_overlap import rbbox_nms

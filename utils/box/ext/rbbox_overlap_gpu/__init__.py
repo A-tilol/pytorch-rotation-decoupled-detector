f34271e0b789c16281314db974c6f01e804955e8
igarashi <igarashi.shunta@canon-elec.co.jp>
2021-12-16 10:20:54 +0900

    update

--- utils/box/ext/rbbox_overlap_gpu/__init__.py
+++ utils/box/ext/rbbox_overlap_gpu/__init__.py
@@ -1,2 +1,10 @@
+import sys, os
+print(os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_cpu"))
+sys.path.append(
+    os.path.join(os.path.abspath('.'), "utils/box/ext/rbbox_overlap_gpu"))
+
+from pprint import pprint
+pprint(sys.path)
+
 from rbbox_overlap import rbbox_overlaps as rbbox_iou
 from rbbox_overlap import rotate_gpu_nms as rbbox_nms

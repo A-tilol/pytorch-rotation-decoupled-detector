0747cfebe940caabe0787328e6e589600792fe5c
igarashi <igarashi.shunta@canon-elec.co.jp>
2021-12-16 10:00:45 +0900

    update

--- utils/box/ext/rbbox_overlap_cpu/__init__.py
+++ utils/box/ext/rbbox_overlap_cpu/__init__.py
@@ -1,6 +1,9 @@
 import sys, os
 sys.path.append(os.path.abspath('.'))
 
+from pprint import pprint
+pprint(sys.path)
+
 from rbbox_overlap import rbbox_iou
 from rbbox_overlap import rbbox_iou_1x1
 from rbbox_overlap import rbbox_iou_nxn
